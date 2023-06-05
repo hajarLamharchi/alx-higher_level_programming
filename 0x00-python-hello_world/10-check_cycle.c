5b117e5f9bf6c2a50d27a61db5a55aaf314c1f1f
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-06-05 19:26:25 +0000

    linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -13,12 +13,18 @@ int check_cycle(listint_t *list)
 	listint_t *fast = list;
 	int status = 0;
 
+	if (list == NULL)
+		return (0);
+
 	while (slow != NULL && fast != NULL && fast->next != NULL)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
 		if (slow == fast)
+		{
 			status = 1;
+			break;
+		}
 	}
 	return (status);
 }
