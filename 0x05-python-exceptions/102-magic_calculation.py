3458e81311c9cb32082954cf3717c506ae0a9f19
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-06-26 15:14:09 +0000

    ByteCode

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 def magic_calculation(a, b):
     result = 0
-    for i in range(103):
+    for i in range(1, 4):
         try:
             if i > a:
                 raise Exception("Too far")
