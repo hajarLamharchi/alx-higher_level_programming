d4706d5f0afb98fd267dc25c7b636186d1fa829b
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-06-27 14:13:27 +0000

    My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 class Square:
     """This class is an empty class that define a square
+        Attributes:
+        ----------
+        None
+        Methods:
+        -------
+        None
     """
     pass
