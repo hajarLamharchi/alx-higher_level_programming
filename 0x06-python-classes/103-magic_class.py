d95b2ab60ac1ced4e1fe71438269fa1f2a0f454d
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-06-28 11:50:32 +0000

    ByteCode

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 import math
-""" This class defines a circle
-"""
+""" This class defines a circle """
 
 
 class MagicClass:
-    """ This class defines a circle by
-        Attribute
-        radius (int or float): the radius of the circle
-    """
+    """ This class defines a circle """
     def __init__(self, radius):
         """ Initializes a new instance of the Class """
         self.__radius = 0
