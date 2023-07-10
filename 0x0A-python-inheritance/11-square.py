9fbd8e77bb84e9814a74f9c62741cbbaf1ba3912
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-07-10 16:19:34 +0000

    task11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """ This module defines the class Square
     that inherits from Rectangle
 """
@@ -15,3 +16,7 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def __str__(self):
+        """ returns a string representation of the square """
+        return "[Square] {}/{}".format(self.__size, self.__size)
