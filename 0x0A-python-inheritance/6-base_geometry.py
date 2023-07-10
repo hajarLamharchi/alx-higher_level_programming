86dba49f1c83fc2aebbdfdb702901411bec53494
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-07-10 13:24:22 +0000

    task6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This module defines the class BaseGeometry """
+
+
+class BaseGeometry:
+    """ This class is empty """
+    def area(self):
+        """ raises an exception """
+        raise Exception('area() is not implemented')
