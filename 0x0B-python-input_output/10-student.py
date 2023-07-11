1b59f28bb89642e82c149565c63b1128938f6e04
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-07-11 16:27:12 +0000

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" This module defines the class Student """
+
+
+class Student:
+    """ This class defines a student by
+    Attributes:
+        first_name: string
+        last_name: string
+        age: int
+    """
+    def __init__(self, first_name, last_name, age):
+        """ Initializes the attributes of the class """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Rtrieves a dictionary representation of a student """
+        if type(attrs) == list and all(type(i) == str for i in attrs):
+            for i in attrs:
+                if hasattr(self, i):
+                    return {i: getattr(self, i)}
+        return self.__dict__
