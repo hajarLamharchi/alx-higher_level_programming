f6a4c3d0a725998fcffa131cc5044411ff446750
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-09-11 19:36:20 +0000

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
+if (process.argv[2] === undefined || process.argv[2] === null) {
   console.log('0');
 } else if (process.argv.length === 3) {
   console.log('0');
