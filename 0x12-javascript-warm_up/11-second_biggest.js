da9301eb4461475cb9d101f68a3c2dc75060eedd
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-09-11 22:10:54 +0000

    fixing task11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,18 +1,8 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || process.argv[2] === null) {
-  console.log(0);
-} else if (process.argv.length === 3) {
+const arg = process.argv;
+if (arg.length <= 3) {
   console.log(0);
 } else {
-  let max = process.argv[2];
-  let secondMax = 0;
-  for (let i = 2; i < process.argv.length; i++) {
-    if (process.argv[i] > max) {
-      secondMax = max;
-      max = process.argv[i];
-    } else if (process.argv[i] > secondMax && process.argv[i] <= max) {
-      secondMax = process.argv[i];
-    }
-  }
-  console.log(secondMax);
+  arg.sort();
+  console.log(arg[arg.length - 2]);
 }
