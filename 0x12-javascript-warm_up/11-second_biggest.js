4f1810288d28928453977244968cc6599bc19b19
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-09-11 19:47:41 +0000

    fix task11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 if (process.argv[2] === undefined || process.argv[2] === null) {
-  console.log('0');
+  console.log(0);
 } else if (process.argv.length === 3) {
-  console.log('0');
+  console.log(0);
 } else {
   let max = process.argv[2];
-  let secondMax = process.argv[2];
+  let secondMax = 0;
   for (let i = 2; i < process.argv.length; i++) {
     if (process.argv[i] > max) {
       secondMax = max;
