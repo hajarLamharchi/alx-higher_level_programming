ce3c89c1f15d767d4ef04e60017dbca702d88fdb
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-09-12 13:37:10 +0000

    rectangle 2 fix

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      return new Object();
-    } else {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
