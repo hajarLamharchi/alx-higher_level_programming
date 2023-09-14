60bf80e2bd8530243c55f37e96e0c46595e88a52
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-09-14 19:28:45 +0000

    get state by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This module defines the function get_state_name()"""
+import MySQLdb
+import sys
+
+
+def get_state_name(username, password, name, state):
+    """This function displays all values of state that matches the argument"""
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=username,
+            passwd=password,
+            db=name
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id",
+                (state,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    name = sys.argv[3]
+    state = sys.argv[4]
+    get_state_name(username, password, name, state)
