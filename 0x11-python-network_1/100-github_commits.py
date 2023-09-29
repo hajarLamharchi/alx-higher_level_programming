fd928344a9832433ba783d8bb6f1b0562664ed0b
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-09-29 21:20:42 +0000

    interview

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,4 +4,16 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = f'
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = f'https://api.github.com/repos/{owner}/{repo}/commits'
+
+    r = requests.get(url)
+    data = r.json()
+    count = 0
+    for k in data:
+        count = count + 1
+        if count == 11:
+            break
+        print('{}: {}'.format(k.get('sha'),
+                              k.get('commit').get('author').get('name')))
