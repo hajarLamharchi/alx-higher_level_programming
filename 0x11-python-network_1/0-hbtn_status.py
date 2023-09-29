1390811efbad96f70ee133d181184e28a5034451
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-09-29 13:58:08 +0000

    fetch url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
         html = r.read()
     print("Body response:")
-    print("\t-type: {}".format(type(html)))
-    print("\t-content: {}".format(html))
-    print("\t-utf8 content: {}".format(html.decode('utf8')))
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf8')))
