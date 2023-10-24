659802bf4b066c2a9b7af78be0caf633eafbfc0f
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-10-24 12:33:37 +0000

    semistandard fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-const fs = require('fs')
+const fs = require('fs');
 
-const file_path = process.argv[2]
-fs.readFile(file_path, 'utf-8', (err, data) => {
-	if (err) {
-		console.error(err);
-		return;
-	}
-	console.log(data);
+const filePath = process.argv[2];
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
