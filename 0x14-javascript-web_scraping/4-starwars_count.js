b6277aed3bd0fa3aee47d45289460193858b5d8d
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-10-24 14:28:43 +0000

    task4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,17 +5,16 @@ const request = require('request');
 const url = process.argv[2];
 const options = { json: true };
 
-let c = 0;
-
 request(url, options, (err, response, body) => {
   if (err) {
     console.error(err);
   }
-  const results = body.results;
-  for (let i = 0; i < body.count; i++) {
-    if (results[i].characters.some((str) => str.endsWith('/18/'))) {
-      c += 1;
+  const countChar = body.results.reduce((count, result) => {
+    if (result.characters.some((str) => str.endsWith('/18/'))) {
+      return count + 1;
     }
-  }
-  console.log(c);
+    return count;
+  }, 0);
+
+  console.log(countChar);
 });
